dd1e3a3f4e89ca5109f026b41cb3c3d4bf08e61b
inayat-kaur <2020csb1088@iitrpr.ac.in>
2023-07-18 21:37:48 +0530

    TicTacToe with graphics
    Added graphic interface to the TicTacToe game using Java Swing and AWT.

--- Player.java
+++ Player.java
@@ -1,28 +1,23 @@
 //Inayat Kaur
 //2020csb1088
 
-import java.util.Scanner;
-
-/**This class imitates a player in real world**/
-public class Player{
-	/**The variables store the mark ('O','X') and number (1 or 2) of the player**/
+/** This class imitates a player in real world **/
+public class Player {
+	/** The variables store the mark ('O','X') and number (1 or 2) of the player **/
 	protected char mark;
 	protected int number;
-	
-	public Player(char mark, int number){	//Constructor
+
+	public Player(char mark, int number) { // Constructor
 		this.mark = mark;
 		this.number = number;
 	}
-	
-	/**Scanning input from user for the move and asking game class to make the move**/
-	public void makeMove(Game game){		
-		Scanner input = new Scanner(System.in);
-		int choice = input.nextInt();
+
+	/**
+	 * Scanning input from user for the move and asking game class to make the move
+	 **/
+	public void makeMove(Game game) {
 		Display dis = new Display();
-		while(!game.getOptions().contains(choice)){
-			dis.invalid();
-			choice = input.nextInt();
-		}
-		game.makeMove(choice,mark);
+		int choice = dis.makeMove(game.getMatrix(), this.number);
+		game.makeMove(choice, mark);
 	}
 }
